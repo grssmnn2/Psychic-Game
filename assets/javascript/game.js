ebbc323625959e30dd8fbb612b9330c0290bf8f7
Cecily Grossmann <grssmnn2@gmail.com>
2017-11-28 15:29:20 -0600

    more javascript changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,28 +6,36 @@ var guessesLeft = 10;
 var guessesList =[];
 var userChoice= null;
 // Generate random computer choice and store it in variable computerChoice
-var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length())];
+var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
 
 // When the user presses a keyboard key, computer should run this function
 document.onkeypress = function(event){
 	// user choice is whatever key was pressed
-	var userChoice = String.fromCharCode(event.key).toLowerCase();
+	var userChoice = String.fromCharCode(event.keyCode).toLowerCase();
 	// adds user guess to list
 	guessesList.push(userChoice);
 
 	// if user chooses computer guess, increase wins by 1
-	if (userChoice === computerChoice) {
+	if (userChoice === computerChoice){
 		wins++;
+		guessesList.push(userChoice);
+		// prompt computer to choose a new random number
+		var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
 	}
+	// if no more guesses and still not choosing computer choice, increase Losses by 1, reset guesses left and empty guesses list
+	else if (guessesLeft === 0 && userChoice !== computerChoice){
+		losses++;
+		var guessesLeft = 10;
+		var guessesList = [];
+		// prompt computer to choose a new random number
+		var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
 
-		// if user does not choose computer guess, decrease guesses left by 1
-	else {
-		guessesLeft--;
 	
 	}
-		// if guesses reaches 0, increase losses by 1
-	if (guessesLeft === 0){
-		losses++;
+		// if user choice does not equal computer choice, decrease guesses by 1 and add guess to list
+	else (userChoice !== computerChoice){
+		guesses--;
+		guessesList.push(userChoice);
 	}
 	
 }
