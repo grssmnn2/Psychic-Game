3b75937bc8ac0c52c55a25053bf572123a87fa12
Cecily Grossmann <grssmnn2@gmail.com>
2017-11-28 14:35:00 -0600

    scrambled some more JS around to try

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,34 +6,36 @@ var guessesLeft = 10;
 var guessesList =[];
 var userChoice= null;
 // Generate random computer choice and store it in variable computerChoice
-var computerChoice = alphabet[Math.floor(Math.random()*alphabet.length)];
+var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length())];
 
 // When the user presses a keyboard key, computer should run this function
 document.onkeypress = function(event){
 	// user choice is whatever key was pressed
 	var userChoice = String.fromCharCode(event.key).toLowerCase();
+	// adds user guess to list
+	guessesList.push(userChoice);
 
 	// if user chooses computer guess, increase wins by 1
-	if (userChoice === computerChoice){
+	if (userChoice === computerChoice) {
 		wins++;
-		document.getElementById('wins').innerHTML= "Wins: " + wins;
+	}
 
-	}
-	// if user does not choose computer guess, decrease guesses left by 1
-	else if (userChoice !== computerChoice) {
+		// if user does not choose computer guess, decrease guesses left by 1
+	else {
 		guessesLeft--;
-		// store values that have been guessed in a list
-		guessesList.push(userChoice);
-		document.getElementById('guessesLeft').innerHTML= "Guesses Left: " + guessesLeft;
-		document.getElementById('guessesList').innerHTML = "Guesses so Far: " + guessesList;
+	
 	}
-	// if guesses reaches 0, increase losses by 1
-	else if (guessesLeft === 0){
+		// if guesses reaches 0, increase losses by 1
+	if (guessesLeft === 0){
 		losses++;
-		document.getElementById('losses').innerHTML= "Losses: " + losses;
 	}
+	
 }
 
+document.getElementById('wins').innerHTML= "Wins: " + wins + "<br>";
+document.getElementById('losses').innerHTML= "Losses: " + losses + "<br>";
+document.getElementById('guessesLeft').innerHTML= "Guesses Left: " + guessesLeft + "<br>";
+document.getElementById('guessesList').innerHTML = "Guesses so Far: " + guessesList + "<br>";
 
 
 
