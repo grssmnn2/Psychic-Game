c38e12e66e9c6f4f5ef99ec31693ae0c1e7ebb98
Cecily Grossmann <grssmnn2@gmail.com>
2017-11-28 22:09:10 -0600

    adding changes from class

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,14 +4,15 @@ var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
 var guessesList =[];
-var userChoice= null;
-// Generate random computer choice and store it in variable computerChoice
-var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+
 
 // When the user presses a keyboard key, computer should run this function
-document.onkeypress = function(event){
+document.onkeyup = function(event) {
 	// user choice is whatever key was pressed
-	var userChoice = String.fromCharCode(event.keyCode).toLowerCase();
+	var userChoice = event.key;
+	// Generate random computer choice and store it in variable computerChoice
+	var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+	
 	// adds user guess to list
 	guessesList.push(userChoice);
 
@@ -19,16 +20,18 @@ document.onkeypress = function(event){
 	if (userChoice === computerChoice){
 		wins++;
 		guessesList.push(userChoice);
-		// prompt computer to choose a new random number
+		// prompt computer to choose a new random number and reset code
 		var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+		var guessesLeft = 10;
+		var guessesList = [];
 	}
 	// if no more guesses and still not choosing computer choice, increase Losses by 1, reset guesses left and empty guesses list
 	else if (guessesLeft === 0 && userChoice !== computerChoice){
 		losses++;
+		// prompt computer to choose a new random number and reset code
+		var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
 		var guessesLeft = 10;
 		var guessesList = [];
-		// prompt computer to choose a new random number
-		var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
 
 	
 	}
@@ -37,13 +40,26 @@ document.onkeypress = function(event){
 		guesses--;
 		guessesList.push(userChoice);
 	}
+
+	var html = 
+		"<p> Wins: " + wins + "</p>" +
+		"<p> Losses: " + losses + "</p>" +
+		"<p> Guesses Left: " + guessesLeft + "</p>" +
+		"<p> Guesses so Far: " + guessesList + "</p>";
+
+		document.querySelector("#game").innerHTML = html;
+
+
+// 	document.getElementById(#game).innerHTML= `
+// 	Wins: ${wins}
+// 	Losses: ${losses}
+// 	Guesses Left: ${guessesLeft}
+// 	Guesses so Far: ${guessesList}
+
+// `;
 	
 }
 
-document.getElementById('wins').innerHTML= "Wins: " + wins + "<br>";
-document.getElementById('losses').innerHTML= "Losses: " + losses + "<br>";
-document.getElementById('guessesLeft').innerHTML= "Guesses Left: " + guessesLeft + "<br>";
-document.getElementById('guessesList').innerHTML = "Guesses so Far: " + guessesList + "<br>";
 
 
 
