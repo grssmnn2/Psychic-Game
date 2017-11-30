c93b5a37516d8d517877e0ad9213d44146af7e5b
Cecily Grossmann <grssmnn2@gmail.com>
2017-11-30 10:16:21 -0600

    minor changes to JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,19 +21,19 @@ document.onkeyup = function(event) {
 	// Generate random computer choice and store it in variable computerChoice
 	var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
 	
-	// check if user input is alphabetical
-	// document.onkeyup = function(event){
- //    if (event.key >= 65 && event.key <=90){
-    // guessesList.push(userChoice);
- //    	}
-	// };
+			// check if user input is alphabetical
+			// document.onkeyup = function(event){
+ // 		   if (event.key >= 65 && event.key <=90){
+    		// guessesList.push(userChoice);
+ //    		}
+	// 	};
 	// 			
 					// reset function to call to reset game
-					// function reset() {
+	// 			function reset() {
  // -				var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
  // -				var guessesLeft = 10;
  // -				var guessesList = [];
- // -				}
+ // -			}
 
 			// if user chooses computer guess, increase wins by 1
 			if (userChoice === computerChoice){
@@ -43,22 +43,26 @@ document.onkeyup = function(event) {
 				// prompt computer to choose a new random number and reset code
 				guessesLeft=10;
 				guessesList = [];
+				var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
 				
 				
 			}
-			
+
+			// if incorrect guess, decrease guesses by 1 and add guess to list
 			else {
 				guessesLeft--;
 				guessesList.push(userChoice);
 				
 				}
 
-			// if user choice does not equal computer choice, decrease guesses by 1 and add guess to list
+			//if no more guesses, increase losses by 1 
 			if (guessesLeft ===0){
 				losses++;
 				alert("Everyone loses sometimes. Try again?");
+				// prompt computer to choose a new random number and reset code
 				guessesLeft=10;
 				guessesList = [];
+				var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
 				
 			}
 
